7ff8f4e1c7a43c563066914c8f95ea0d5ee7785b
Jerico151 <skirillov21@yandex.ru>
2020-04-13 20:48:20 +0300

    Initial commit

---
+++ src/test/java/AlertTest.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class AlertTest{
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @BeforeTest
+    public void setUP () {
+        System.setProperty("webdriver.chrome.driver", "c:\\Users/user/Desktop/chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 100 , 0);
+    }
+    @AfterTest
+    public void quitBrowser () {
+        driver.quit();
+    }
+    @Test
+    public void alertTest () {
+
+        //find and click on submit
+        driver.get("http://demo.guru99.com/test/delete_customer.php");
+        driver.findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td[2]/input")).sendKeys("user");
+        driver.findElement(By.xpath("/html/body/form/table/tbody/tr[3]/td[2]/input[1]")).click();
+
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert =driver.switchTo().alert();
+
+        String expectedAlertness = "Do you really want to delete this Customer?";
+        String actualAlertness = alert.getText();
+        Assert.assertEquals(actualAlertness, expectedAlertness);
+        alert.accept();
+
+        Alert alert2 = driver.switchTo().alert();
+        String expectedAlertness2 = "Customer Successfully Delete!";
+        String actualAlertness2 = alert2.getText();
+        Assert.assertEquals(actualAlertness2, expectedAlertness2);
+        alert2.accept();
+    }
+}
